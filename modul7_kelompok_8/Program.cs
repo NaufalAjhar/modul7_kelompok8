b3528db5404512c6b7d0a9c6c9a56d578bdebf69
NaufalAjhar <74417421+NaufalAjhar@users.noreply.github.com>
2024-04-06 08:52:09 +0800

    inisialisasi project

---
+++ modul7_kelompok_8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul7_kelompok_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
